fcad62e3436529a4a8540911675ce8e1ae7febb5
MouayedAmrr <moied.amr15503@gmail.com>
2025-07-09 17:24:14 +0300

    Project structure created

--- lib/src/theme/app_theme.dart
+++ lib/src/theme/app_theme.dart
@@ -1,13 +1,3 @@
-import 'dart:ui';
-
 abstract class AppTheme{
-  static const Color primaryColor = Color(0xFF8E6CEF);
-  static const Color bgLightColor = Color(0xFFFFFFFF);
-  static const Color lightGrey = Color(0xFFF4F4F4);
-  static const Color bgDarkColor = Color(0xFF1D182A);
-  static const Color darkGrey = Color(0xFF342F3F);
-  static const Color orange = Color(0xFFEC6D26);
-  static const Color red = Color(0xFFEC6D26);
-  static const Color yellow = Color(0xFFF4BD2F);
-  static const Color blue = Color(0xFF4468E5);
+
 }
