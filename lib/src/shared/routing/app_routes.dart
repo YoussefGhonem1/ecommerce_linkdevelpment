e1573f53ad3053bb41ecee871929412fa1575735
MouayedAmrr <moied.amr15503@gmail.com>
2025-07-13 19:15:35 +0300

    updates app routes

--- lib/src/shared/routing/app_routes.dart
+++ lib/src/shared/routing/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_app/src/features/forget_password/screens/forget_password_screen.dart';
 import 'package:ecommerce_app/src/features/forget_password/screens/return_to_login_screen.dart';
 import 'package:ecommerce_app/src/features/on_boarding/presentation/pages/on_boarding_page.dart';
+import 'package:ecommerce_app/src/features/splash/presentation/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class Routes{
@@ -13,7 +14,7 @@ class AppRoutes {
   static Route? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.initial:
-        return MaterialPageRoute(builder: (_) => const ForgetPasswordScreen());
+        return MaterialPageRoute(builder: (_) => const SplashPage());
       case Routes.onBoarding:
         return MaterialPageRoute(builder: (_) => const OnBoardingPage());
       case Routes.forgetPassword:
