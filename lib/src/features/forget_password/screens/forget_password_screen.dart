65f0fc42409005de24e7c9f849ace3088ad70193
MouayedAmrr <moied.amr15503@gmail.com>
2025-07-13 19:07:27 +0300

    forgetPass and returnToLogin screens added

---
+++ lib/src/features/forget_password/screens/forget_password_screen.dart
@@ -0,0 +1,48 @@
+import 'package:ecommerce_app/src/shared/components/custom_button.dart';
+import 'package:ecommerce_app/src/shared/components/custom_text_field.dart';
+import 'package:flutter/material.dart';
+
+import '../../../shared/routing/app_routes.dart';
+
+class ForgetPasswordScreen extends StatefulWidget {
+  const ForgetPasswordScreen({super.key});
+
+  @override
+  State<ForgetPasswordScreen> createState() => _ForgetPasswordScreenState();
+}
+
+class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
+  final _forgetPassController = TextEditingController();
+
+  @override
+  void dispose() {
+    _forgetPassController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(), // until I have create acc screen to nav back.
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Forget Password",style:theme.textTheme.headlineMedium),
+            SizedBox(height: 20),
+            CustomTextField(
+                controller: _forgetPassController,
+                hintText: "Enter Email address"),
+            SizedBox(height: 20),
+            CustomButton(text: "Continue", onPressed: (){
+              Navigator.pushNamed(context, Routes.returnToLogin);
+            })
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/features/forget_password/screens/return_to_login_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/app_images.dart';
+
+class ReturnToLoginScreen extends StatelessWidget {
+  const ReturnToLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            Assets.restPassImage,
+            width: 100,
+          ),
+          SizedBox(height: 20),
+          Text("We Sent you an Email to reset your password.",
+            style: theme.textTheme.headlineSmall,
+            textAlign: TextAlign.center,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: ElevatedButton(
+              onPressed: (){
+                //Until I had login screen to nav into.
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF8E6CEF),
+                foregroundColor: Colors.white,
+                disabledBackgroundColor: const Color(0xFF8E6CEF).withOpacity(0.6),
+                textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 24),
+                child: Text("Return to Login"),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
