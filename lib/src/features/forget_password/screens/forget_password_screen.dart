51abaa924372b1707e87f7feffc078df74f4138e
MouayedAmrr <moied.amr15503@gmail.com>
2025-07-16 02:47:44 +0300

    last project update

---
+++ lib/src/features/forget_password/screens/forget_password_screen.dart
@@ -0,0 +1,66 @@
+import 'package:ecommerce_app/src/shared/components/custom_button.dart';
+import 'package:ecommerce_app/src/shared/components/custom_text_field.dart';
+import 'package:flutter/material.dart';
+
+import '../../../shared/routing/app_routes.dart';
+import '../../../shared/theme/app_colors.dart';
+
+class ForgetPasswordScreen extends StatefulWidget {
+  const ForgetPasswordScreen({super.key});
+
+  @override
+  State<ForgetPasswordScreen> createState() => _ForgetPasswordScreenState();
+}
+
+class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
+  final _forgetPassController = TextEditingController();
+
+  @override
+  void dispose() {
+    _forgetPassController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(25),
+            onTap: () => Navigator.of(context).pop(),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: AppColors.lightGrey,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(Icons.arrow_back_ios_new,
+                  size: 16, color: Colors.black),
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 14),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Forget Password", style: theme.textTheme.headlineMedium),
+            SizedBox(height: 20),
+            CustomTextField(
+                controller: _forgetPassController,
+                hintText: "Enter Email address"),
+            SizedBox(height: 20),
+            CustomButton(
+                text: "Continue",
+                onPressed: () {
+                  Navigator.pushNamed(context, Routes.returnToLogin);
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
