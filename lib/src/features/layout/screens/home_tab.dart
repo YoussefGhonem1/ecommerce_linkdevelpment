412fa822fe9e492a64ecc75afa16d0c315e2a586
MouayedAmrr <moied.amr15503@gmail.com>
2025-07-16 03:57:38 +0300

    created nav bar and custom nav bar item

---
+++ lib/src/features/layout/screens/home_tab.dart
@@ -0,0 +1,34 @@
+import 'package:ecommerce_app/src/shared/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../generated/assets.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: Image.asset(Assets.imagesProfileImage),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: AppColors.primaryColor
+                    ),
+                    child: SvgPicture.asset(Assets.iconsCart,width:16,),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
