dcde111eb06cda2bd2795b6932ede00da847f15e
YoussefGhonem1 <youssefghoneem883@gmail.com>
2025-08-16 16:00:14 +0300

    publish version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,10 +26,10 @@ android {
         applicationId = "com.example.ecommerce_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     buildTypes {
